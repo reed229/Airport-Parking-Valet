ee446569aedbe712481ee3a2ac18e33b7ca1460d
Zuqarey <zufarabqary1041@gmail.com>
2026-01-02 00:34:22 +0800

    png class diargram

--- Admin.java
+++ Admin.java
@@ -1,3 +1,26 @@
-public class Admin {
+public class Admin extends User{
+    private String role;
+
+    public Admin(String name, String id, String contact, String role) {
+        super(name, id, contact);
+        this.role = role;
+    }
+
+    public void setRole(String role) {
+        this.role = role;
+    }
+
+    public String getRole() {
+        return role;
+    }
+
+    @Override
+    public String toString() {
+        return super.toString() + 
+               "Role    : " + role;
+    }
+}
+ {
+
     
 }
