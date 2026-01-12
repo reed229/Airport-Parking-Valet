3dd7c76f745665e995b6a58bb0d7d6c89cade188
Zuqarey <zufarabqary1041@gmail.com>
2026-01-12 19:06:37 +0800

    Add TODO

--- Admin.java
+++ Admin.java
@@ -1,10 +1,14 @@
-public class Admin extends User{
+public class Admin extends User {
     private String password;
 
     public Admin(String name, String id, String contact, String password) {
         super(name, id, contact);
         this.password = password;
     }
+
+    // TODO: Add Valet
+
+    // TODO: Remove Valet
 
     public void setPassword(String password) {
         this.password = password;
