02b29b05abaed2ab8bcabf28dcd08adffa74126e
Zuqarey <zufarabqary1041@gmail.com>
2026-01-12 19:36:52 +0800

    insert van class into folder

--- Van.java
+++
@@ -1,5 +0,0 @@
-public class Van extends Vehicle {
-	public Van( String vehicleType, String brand, String vehicleBrand, String roadTax, ParkingLot parkingLot, String platNum) {
-		super(vehicleType, brand, vehicleBrand, roadTax, parkingLot, platNum);
-	}
-}
