329ee9f263a44911640a2c6d94646ae3897795ee
Zuqarey <zufarabqary1041@gmail.com>
2026-01-12 19:19:52 +0800

    Complete Vehicle Class Definition

--- Vehicle.java
+++ Vehicle.java
@@ -1,3 +1,57 @@
-public class Vehicle {
-    
+public abstract class Vehicle {
+	protected String vehicleType;
+	protected String brand;
+	protected String vehicleBrand;
+	protected String roadTax;
+	protected ParkingLot parkingLot;
+	protected String platNum;
+
+	public Vehicle( String vehicleType, String brand, String vehicleBrand, String roadTax, ParkingLot parkingLot, String platNum) {
+		this.vehicleType = vehicleType;
+		this.brand  = brand;
+		this.vehicleBrand = vehicleBrand;
+		this.roadTax = roadTax;
+		this.parkingLot = parkingLot;
+		this.platNum = platNum;
+	}
+
+	// Setters
+	protected void setVehicleType(String vehicleType) {
+		this.vehicleType = vehicleType;
+	}
+	protected void setBrand(String brand) {
+		this.brand = brand;
+	}
+	protected void setVehicleBrand(String vehicleBrand) {
+		this.vehicleBrand = vehicleBrand;
+	}
+	protected void setRoadTax(String roadTax) {
+		this.roadTax = roadTax;
+	}
+	protected void setParkingLot(ParkingLot parkingLot) {
+		this.parkingLot = parkingLot;
+	}
+	protected void setPlatNum(String platNum) {
+		this.platNum = platNum;
+	}
+
+	// Getters
+	protected String getVehicleType() {
+		return vehicleType;
+	}
+	protected String getBrand() {
+		return brand;
+	}
+	protected String getVehicleBrand() {
+		return vehicleBrand;
+	}
+	protected String getRoadTax() {
+		return roadTax;
+	}
+	protected ParkingLot getParkingLot() {
+		return parkingLot;
+	}
+	protected String getPlatNum() {
+		return platNum;
+	}
 }
