1a4496a3ab51fc4710133c0c46a28e946559e8c9
Zuqarey <zufarabqary1041@gmail.com>
2026-01-12 19:34:24 +0800

    insert car class into folder

--- Car.java
+++
@@ -1,5 +0,0 @@
-public class Car extends Vehicle {
-	public Car( String vehicleType, String brand, String vehicleBrand, String roadTax, ParkingLot parkingLot, String platNum) {
-        super(vehicleType, brand, vehicleBrand, roadTax, parkingLot, platNum);
-    }
-}
