ab16b799c28f3ad6510486a32f7078697258e355
Zuqarey <zufarabqary1041@gmail.com>
2026-01-12 19:06:44 +0800

    Create Car Class

--- Car.java
+++ Car.java
@@ -1,4 +1,5 @@
-public class Car {
-
-    
+public class Car extends Vehicle {
+	public Car( String vehicleType, String brand, String vehicleBrand, String roadTax, ParkingLot parkingLot, String platNum) {
+        super(vehicleType, brand, vehicleBrand, roadTax, parkingLot, platNum);
+    }
 }
