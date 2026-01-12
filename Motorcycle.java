28003025e3d680d7e3a8134b36be0881f046f121
Zuqarey <zufarabqary1041@gmail.com>
2026-01-12 19:17:52 +0800

    Create Motorcycle class

--- Motorcycle.java
+++ Motorcycle.java
@@ -1,3 +1,5 @@
-public class Motorcycle {
-    
+public class Motorcycle extends Vehicle {
+	public Motorcycle( String vehicleType, String brand, String vehicleBrand, String roadTax, ParkingLot parkingLot, String platNum) {
+		super(vehicleType, brand, vehicleBrand, roadTax, parkingLot, platNum);
+	}
 }
