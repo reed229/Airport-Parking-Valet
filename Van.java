7dcf9429a3a778f8df7845536eb25dd0a10bc506
Zuqarey <zufarabqary1041@gmail.com>
2026-01-12 19:19:05 +0800

    Create Van Class

--- Van.java
+++ Van.java
@@ -1,3 +1,5 @@
-public class Van {
-    
+public class Van extends Vehicle {
+	public Van( String vehicleType, String brand, String vehicleBrand, String roadTax, ParkingLot parkingLot, String platNum) {
+		super(vehicleType, brand, vehicleBrand, roadTax, parkingLot, platNum);
+	}
 }
