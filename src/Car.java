99e48819321dd1cda781c4b2d8e8a89e100ca50a
Zuqarey <zufarabqary1041@gmail.com>
2026-01-16 04:38:53 +0800

    minor change

--- src/Car.java
+++ src/Car.java
@@ -3,4 +3,13 @@ public class Car extends Vehicle {
 	public Car( String vehicleType, String brand, String vehicleBrand, String roadTax, ParkingLot parkingLot, String platNum) {
         super(vehicleType, brand, vehicleBrand, roadTax, parkingLot, platNum);
     }
+    @Override
+    public double vehicleCost() {
+        return 65.0;
+    }
+    @Override
+    public String toString() {
+        return super.toString() + 
+               "Vehicle Cost per Hour: RM65.0\n";
+    }
 }
