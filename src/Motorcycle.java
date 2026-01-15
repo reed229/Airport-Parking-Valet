997101ccfeb7b33ece3c345838545d2b45a6f47c
Zuqarey <zufarabqary1041@gmail.com>
2026-01-16 04:39:09 +0800

    minor change

--- src/Motorcycle.java
+++ src/Motorcycle.java
@@ -3,4 +3,9 @@ public class Motorcycle extends Vehicle {
 	public Motorcycle( String vehicleType, String brand, String vehicleBrand, String roadTax, ParkingLot parkingLot, String platNum) {
 		super(vehicleType, brand, vehicleBrand, roadTax, parkingLot, platNum);
 	}
+		
+	@Override
+	public double vehicleCost() {
+		return 55.0;
+	}
 }
