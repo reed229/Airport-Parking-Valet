6c479d913617dd81395146cba9baed304a942cc2
reed229 <fareedirfan05@gmail.com>
2026-01-17 02:57:54 +0800

    remove roadTax in van class

--- src/Van.java
+++ src/Van.java
@@ -1,7 +1,7 @@
 package src;
 public class Van extends Vehicle {
-	public Van( String vehicleType, String brand, String vehicleBrand, String roadTax, ParkingLot parkingLot, String platNum) {
-		super(vehicleType, brand, vehicleBrand, roadTax, parkingLot, platNum);
+	public Van( String vehicleType, String brand, String vehicleBrand, ParkingLot parkingLot, String platNum) {
+		super(vehicleType, brand, vehicleBrand, parkingLot, platNum);
 	}
 		@Override
 	public double vehicleCost() {
