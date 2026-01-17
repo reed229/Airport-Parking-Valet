0b5527aadecdb74b2a290451cd0cc86a4c446216
reed229 <fareedirfan05@gmail.com>
2026-01-18 05:06:55 +0800

    adjust toString

--- src/Admin.java
+++ src/Admin.java
@@ -31,7 +31,7 @@ public class Admin extends User {
     @Override
     public String toString() {
         return super.toString() + 
-               "Role    : " + password;
+               "Role: " + password;
     }
 }
  
